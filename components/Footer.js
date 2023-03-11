6b88c350e74f4e9d8987e2ce0ddf847e83764ba5
simplomaticindia <123868966+simplomaticindia@users.noreply.github.com>
2023-03-11 09:07:05 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,6 @@ function Footer() {
         <p>India</p>
       </div>
 
-      <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
-        <div className="flex link justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2 font-Ubuntu">
-          <img src="github-brands.png" className="h-5  px-1 "></img>{" "}
-          <a href="https://myaccount.google.com/security-checkup">
-            {" "}
-            Github.com/cosmicwanderer7{" "}
-          </a>
-        </div>
 
         <div className="flex justify-center text-sm space-x-5 whitespace-nowrap md:justify-self-start">
           <a href="https://about.google">
